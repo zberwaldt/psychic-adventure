a84fd937abbcf662f042f8c244a77d72f32dba56
Zach Berwaldt <zberwaldt@tutamail.com>
2021-06-25 08:40:41 -0400

    remove react-html-parser. doesn't render immediately? changed to dangerouslysetinnerhtml in previewer. all tests passing

--- src/components/Previewer.tsx
+++ src/components/Previewer.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
-import parse from 'html-react-parser';
 import './Previewer.css';
+
 type PreviewerProps = {
     content?: any
 };
 
 const Previewer = ({ content }: PreviewerProps) => {
 
-  let parsed = parse(content);
-
   return (
-    <div id="preview">
-      { parsed }
-    </div>
-  )
+    <div id="preview" dangerouslySetInnerHTML={{ __html: content }} />
+  );
 
 }
 
